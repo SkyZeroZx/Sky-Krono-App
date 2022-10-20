6413f4772f77a74262f0be22e7af30646cebbd66
SkyZeroZx <73321943+SkyZeroZx@users.noreply.github.com>
2022-10-19 22:56:01 -0500

    Add WorkFlow Test

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,19 +1,33 @@
 import { defineConfig } from 'cypress';
 
 export default defineConfig({
+  projectId: 'rncrpx',
   videosFolder: 'cypress/videos',
   screenshotsFolder: 'cypress/screenshots',
   fixturesFolder: 'cypress/fixtures',
   video: true,
+  viewportHeight: 844,
+  viewportWidth: 390,
+  experimentalStudio: true,
+  waitForAnimations : true,
+  defaultCommandTimeout: 30000,
   env: {
+    mobileViewportWidthBreakpoint: 600,
     baseUrl: 'https://api-sky-krono.skyzerozx.com',
     service: {
       visit: 'http://localhost:4200',
       api: 'https://api-sky-krono.skyzerozx.com',
     },
+    dateRange: {
+      dateInit: '21',
+      dateEnd: '25',
+    },
     users: {
       admin: {
         username: 'skyzerobot64@gmail.com',
+        name: 'SkyBot',
+        fatherLastName: 'paterno',
+        motherLastName: 'materno',
         password: 'Admin1',
         role: 'admin',
       },
@@ -27,9 +41,13 @@ export default defineConfig({
         password: 'PjjiN5UvZ9',
         role: 'admin',
       },
+      reset: {
+        username: 'skyzerobot64CTesting@gmail.com',
+      },
     },
   },
   e2e: {
+    experimentalStudio: true,
     //   supportFile: 'cypress/support/commands.ts',
     setupNodeEvents(on, config) {
       require('@cypress/code-coverage/task')(on, config);
